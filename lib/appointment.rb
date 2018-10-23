dae83123506a014a4324a59a42bb35fa0d19878f
Sierra McCahon <sierrakatemitchell7@gmail.com>
2018-10-23 01:40:27 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,21 @@
+class Appointment
+
+
+attr_accessor :name, :artist, :genre
+  
+  @@all = []
+
+
+def initialize(name, artist, genre)
+   @mame = name
+   @artist = artist 
+   @genre = genre
+   @@all << self
+end 
+  
+ 
+  def self.all
+    @@all
+  end 
+  
+end 
