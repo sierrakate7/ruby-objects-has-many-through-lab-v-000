7c26d5d205bc0689a039dd86870c462e740a508a
Sierra McCahon <sierrakatemitchell7@gmail.com>
2018-10-23 01:07:44 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -4,11 +4,6 @@ class Genre
   
 @@all = []
 
-def initialize(name)
-    @name = name 
-    @@all << self
-  end
-
 
 def initialize(artist, song)
     @artist = artist  
@@ -19,23 +14,15 @@ def initialize(artist, song)
  def songs
    Song.all.select do |song|
       song.Genre == self
->>>>>>> 13e156292a7399484e42fcf3d17f6236ae8127fe
     end
   end
   
   def artists
-<<<<<<< HEAD
     songs.map do |song|
       song.artist
     end
   end
   
-=======
-   Song.all.select do |song|
-      song.Artist == self
-    end
-  end
->>>>>>> 13e156292a7399484e42fcf3d17f6236ae8127fe
 
 def self.all
     @@all
